c51a1e0c1f0251324ea934b203a3c2e1d80eeabc
Pride-Roger <manhav.yuvaraja8@gmail.com>
2024-06-19 17:39:57 +0530

    Add files via upload

---
+++ For git.py
@@ -0,0 +1,7 @@
+Var=int(input('Enter the start value:'))
+Var_1=int(input('Enter the stop value:'))
+Var_2=int(input('Enter the value that needs to multiply other numbers:'))
+
+for z in range(Var,Var_1+1):
+    D=Var_2*z
+    print(Var_2,'*',z,'=',D)
