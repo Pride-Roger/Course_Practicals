6cae83b77b12aa46cfae360d494c95137bd6b487
Pride-Roger <manhav.yuvaraja8@gmail.com>
2024-09-06 17:53:40 +0530

    Add files via upload

---
+++ Pract_15.py
@@ -0,0 +1,10 @@
+def Reminder(V,V_1):
+    print('The reminder from the calculation =', V/V_1)
+
+def Addition(V_2,V_3):
+    print('The sum =', V_2+V_3)
+
+import random as rd
+
+def random(V_4):
+    print(rd.choice(V_4))
---
+++ Pract_16.py
@@ -0,0 +1,7 @@
+import Pract_15 as mod
+
+mod.Reminder(3, 2)
+
+mod.Addition(5, 6)
+
+mod.random(['a', 'b', 'c', 'd'])
