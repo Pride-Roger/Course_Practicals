a18d9946603821246bac7e4738b5f5979a4798bf
Pride-Roger <manhav.yuvaraja8@gmail.com>
2024-09-06 17:53:15 +0530

    Add files via upload

---
+++ Pract_14.py
@@ -0,0 +1,20 @@
+"""class Car:
+    def __init__(self):
+        self.carname='Audi'
+        self.color='Black'
+        self.topspeed='250'
+
+Car_1=Car()
+print(Car_1.carname, Car_1.color, Car_1.topspeed)"""
+
+class Student:
+    def __init__(self, name, grade, section):
+        self.Name=name
+        self.Grade=grade
+        self.GradeSection=section
+        
+Student_1=Student('Clement', '10', 'A')
+Student_2=Student('Watson', '9', 'B')
+Student_3=Student('Jeff', '11', 'A')
+print(Student_1.Name, Student_1.Grade, Student_1.Section)
+#flask
